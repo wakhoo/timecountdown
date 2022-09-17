2590185e59d996d6841ad34f584823161e7ba4fe
전채원(Chaewon Jeon) <qqww212@korea.ac.kr>
2022-09-17 15:48:55 +0900

    Delete main directory

--- main/flask_api/db.py
+++
@@ -1,5 +0,0 @@
-import certifi
-ca=certifi.where()
-from pymongo import MongoClient
-client = MongoClient("mongodb+srv://codnjs:test@cluster0.0frgcjv.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=ca)
-db = client.wakhoo
